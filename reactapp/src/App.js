1ba0097ec1871edefd0b9eb4c91a4811a7df03cb
saifFast <saifullahhakro@gmail.com>
2024-09-21 14:03:09 +0500

    changes in App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useState} from "react";
 
-function App() {
+// function App()
+// {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+//     <h1>React Counter</h1>
+//     <p>{count}</p>
+//     <button onClick={()=> setCount(count  + 1)}>Incerement</button>
+//     <button onClick={()=> setCount(count  - 1)} style={{marginLeft : '10px'}}>Decrement</button>
+
+// </div>
+//   );
+// }
+
+// export default App;
+
+import React, {useState} from "react";
+
+function App()
+{
+  const[todo, setTodo] = useState('');
+  const[todos, setTodos] = useState([]);
+
+  const HandleAddTodo = (e) =>
+  {
+    e.preventDefault(); // Prevent page reload
+    if(todo.trim === '')
+    {
+      return;
+    }
+    setTodos([...todos, todo]);
+    setTodo('');
+  }
+
+  const handleRemoveTodo = (index) =>
+  {
+setTodos(todos.filter((_,i)=> i!==index));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={{ textAlign : 'center', marginTop : '50px'}}>
+<h1>React Todo List</h1>
+
+<form onSubmit={HandleAddTodo}>
+  <input placeholder="Enter Item" type="text" value= {todo} onChange={(e) => setTodo(e.target.value)}>
+  </input>
+  <button type="submit"> Add Task</button>
+</form>
+
+<ul style={{ listStyleType: 'none', padding: 0 }}>
+        {todos.map((task, index) => (
+          <li key={index} style={{ marginTop: '10px' }}>
+            {task}
+            <button
+              onClick={() => handleRemoveTodo(index)}
+              style={{ marginLeft: '10px' }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+
+
+
     </div>
+
+
   );
+
 }
 
 export default App;
