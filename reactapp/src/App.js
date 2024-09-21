2470dfa8270f40cc34dbc1ef1442ce1c4fd3d8a9
saifFast <saifullahhakro@gmail.com>
2024-09-21 14:37:15 +0500

    Added Functional Component with Props

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Greeting from "./Greeting";
+import GreetingWithProp from "./GreetingWithInput";
 
 function App()
 {
-  return (
-    <div className="App">
-  <h1>Hello Saif</h1>
-  <Greeting/>
-  </div>
-
+  return (<div className="App">
+    <h1>Hello Saif</h1>
+    <Greeting/>
+    <GreetingWithProp name= "Prop Bhaiya"/>
+    </div>
   );
 }
 
---
+++ reactapp/src/GreetingWithInput.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function GreetingWithProp(props)
+{
+    return <h1>Props {props.name}</h1>;
+}
+
+export default GreetingWithProp;
