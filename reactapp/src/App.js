50048713e7b643c4f95ffb732369e9f98ee62015
saifFast <saifullahhakro@gmail.com>
2024-09-28 19:27:27 +0500

    Added functional component which returns image

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Greeting from "./Greeting";
 import GreetingWithProp from "./GreetingWithInput";
+import ReturnImage from "./Image";
 
 function App()
 {
@@ -8,6 +9,7 @@ function App()
     <h1>Hello Saif</h1>
     <Greeting/>
     <GreetingWithProp name= "Prop Bhaiya"/>
+    <ReturnImage></ReturnImage>
     </div>
   );
 }
---
+++ reactapp/src/Image.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ReturnImage()
+{
+    return <img src="test.jpg" width={200} height={200}></img>
+}
+
+export default ReturnImage;
